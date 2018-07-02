886d3bd4c9206c571f27a25c94bb98671829281f
Brian Clozel <bclozel@pivotal.io>
2018-07-02 15:35:14 +0200

    Only produce JSON

--- src/main/java/io/spring/demo/sb2upgrade/TeamMemberController.java
+++ src/main/java/io/spring/demo/sb2upgrade/TeamMemberController.java
@@ -1,10 +1,13 @@
 package io.spring.demo.sb2upgrade;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 public class TeamMemberController {
 
 	private final TeamMemberRepository repository;
